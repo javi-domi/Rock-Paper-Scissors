9c76ab445824118f05078d3add231c8e71e445e6
Javier Dominguez <frandomb@gmail.com>
2021-02-03 16:14:38 -0300

    Estructura y estilos terminados

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# Rock Paper Scissors
+
+Rock paper scissors game to play against the computer.
+
+## What I learned:
---
+++ app.js
@@ -0,0 +1,4 @@
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(playerSelection, computerSelection);
